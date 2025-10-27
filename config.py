7735ea29fddd81122ca649cae33f3e5d43c7a342
garpil28 <storegarfield076@gmail.com>
2025-10-27 22:24:09 +0700

    Add main configuration for GarfieldOrderBot
    Add main configuration for GarfieldOrderBot including token, owner ID, default payment method, and shop name.

---
+++ config.py
@@ -0,0 +1,12 @@
+# Konfigurasi utama bot GarfieldOrderBot
+
+BOT_TOKEN = "1234567890:ABCDEF-TOKEN"
+OWNER_ID = 6954401932
+
+DEFAULT_PAYMENT = {
+    "method": "DANA",
+    "account": "081219623569",
+    "name": "Lusiana Kurniawati"
+}
+
+SHOP_NAME = "Garfield Store"
